6d0b0c6fe0b486da492c8a982b5e8793677f219e
wadafacc <schlumpftimo14@gmail.com>
2022-06-17 15:06:07 +0200

    w

---
+++ lua/opts.lua
@@ -0,0 +1,13 @@
+vim.o.number = true
+vim.opt.smarttab = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
+vim.o.hlsearch = true
+vim.o.incsearch = true
+vim.o.smartcase = true
+
+vim.o.fileencoding = 'utf-8'
+vim.o.termguicolors = true
+
